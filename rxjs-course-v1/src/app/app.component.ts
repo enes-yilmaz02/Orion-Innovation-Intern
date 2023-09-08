d39a07a0727ffa191d31c2e2e3b7cfa1b971faaf
enes-yilmaz02 <ey684241@gmail.com>
2023-09-08 10:07:23 +0300

    rxjs

---
+++ rxjs-course-v1/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'rxjs-course-v1';
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    const observable = new Observable((subscriber) => {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+      subscriber.next(4);
+      subscriber.complete();
+
+    });
+
+    observable.subscribe(data => {
+      console.log(data);
+    });
+  }
+}
